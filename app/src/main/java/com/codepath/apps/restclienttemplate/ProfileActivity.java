b727870866aadc18a66101f83ba372f5aad88749
tanvigupta17 <tanvigupta0217@gmail.com>
2017-07-05 20:51:06 -0700

    Profile view UI improvements

--- app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
@@ -1,8 +1,13 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -73,13 +78,58 @@ public class ProfileActivity extends AppCompatActivity {
         tvName.setText(user.name);
         tvHandle.setText("@" + user.screenName);
         tvTagline.setText(user.tagline);
-        tvFollowers.setText(user.followersCount + " Followers");
-        tvFollowing.setText(user.followingCount + " Following");
+
+        String follower_count = truncateNumber(user.followersCount);
+        String following_count = truncateNumber(user.followingCount);
+
+        // Span to make text black
+        ForegroundColorSpan redForegroundColorSpan = new ForegroundColorSpan(Color.rgb(0, 0, 0));
+        // Span to make text bold
+        StyleSpan bold = new StyleSpan(android.graphics.Typeface.BOLD);
+
+        // Use a SpannableStringBuilder so that both the text and the spans are mutable
+        SpannableStringBuilder ssb_a = new SpannableStringBuilder(follower_count);
+        SpannableStringBuilder ssb_b = new SpannableStringBuilder(following_count);
+
+        // Apply the color span
+        ssb_a.setSpan(redForegroundColorSpan, 0, ssb_a.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb_a.setSpan(bold, 0, ssb_a.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        ssb_b.setSpan(redForegroundColorSpan, 0, ssb_b.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb_b.setSpan(bold, 0, ssb_b.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // Add a blank space
+        ssb_a.append(" Followers");
+        ssb_b.append(" Following");
+
+        tvFollowers.setText(ssb_a, TextView.BufferType.EDITABLE);
+        tvFollowing.setText(ssb_b, TextView.BufferType.EDITABLE);
 
         Glide
                 .with(this)
                 .load(user.profileImageUrl)
                 .bitmapTransform(new RoundedCornersTransformation(this, 20, 0))
                 .into(ivProfileImage);
+    }
+
+    public String truncateNumber(float floatNumber) {
+        long million = 1000000L;
+        long billion = 1000000000L;
+        long trillion = 1000000000000L;
+        long number = Math.round(floatNumber);
+        if ((number >= million) && (number < billion)) {
+            float fraction = calculateFraction(number, million);
+            return Float.toString(fraction) + "M";
+        } else if ((number >= billion) && (number < trillion)) {
+            float fraction = calculateFraction(number, billion);
+            return Float.toString(fraction) + "B";
+        }
+        return Long.toString(number);
+    }
+
+    public float calculateFraction(long number, long divisor) {
+        long truncate = (number * 10L + (divisor / 2L)) / divisor;
+        float fraction = (float) truncate * 0.10F;
+        return fraction;
     }
 }
